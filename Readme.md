6ef0b695a3a474f2b5f2e54e2544dec2e488ff90
Shaun Scaling <sscaling@users.noreply.github.com>
2016-12-22 12:25:26 +0000

    First draft of tree rendering

--- Readme.md
+++ Readme.md
@@ -8,6 +8,11 @@
 * SVG / Data-driven documents [https://d3js.org/]()
 * (out of date) [https://github.com/ErikGartner/dTree]()
 
+#### Drawing
+
+* Using CSS: http://thecodeplayer.com/walkthrough/css3-family-tree
+* https://jsplumbtoolkit.com/demos.html
+
 ### Dead
 
 * [https://github.com/mrblueblue/d3-react-family-tree]()
--- react-poc/src/test.js
+++ react-poc/src/test.js
@@ -1,8 +1,57 @@
-function App() {
-    return <div>{ new Date().getTime() }</div>;
+// from dataToTree.js
+var input = {"ids":[1,2],"depth":1,"children":[{"ids":[3,5],"depth":2,"children":[{"ids":[6],"depth":3,"children":[]}]},{"ids":[4,7],"depth":2,"children":[]}]};
+
+//var input = {
+//    "ids": [1],
+//    "depth": 1,
+//    "children": [{"ids": [5], "depth": 2, "children": []}, {"ids": [6], "depth": 3, "children": []}]
+//};
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.idx = 0;
+        console.log(props.tree);
+
+        this.state = {
+            tree: this.buildTree(props.tree)
+        };
+    }
+
+    buildTree(node) {
+        var self = this;
+
+        // array of this node + siblings
+        var nodes = node.ids.map((i) =>
+            <a href="#" key={i}>{i}</a>
+        );
+
+        var children = [];
+        if (node.children.length > 0) {
+            node.children.forEach(function (x) {
+                children.push(self.buildTree(x));
+            });
+        }
+
+        console.log('nodes');
+        console.log(nodes);
+        console.log('children');
+        console.log(children);
+
+        return <li key={ this.idx++ }>
+                { nodes }
+                { children.length > 0 ? <ul>{ children }</ul> : "" }
+            </li>;
+    }
+
+    render() {
+        return <div className="tree"><ul>{ this.state.tree }</ul></div>;
+    }
 }
 
 ReactDOM.render(
-    <App />,
+    <App tree={ input }/>,
     document.getElementById('root')
 );
