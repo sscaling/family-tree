7bd45b23d3e83badec1eb2bcde5a21ae1c34a9c8
Shaun Scaling <sscaling@users.noreply.github.com>
2016-12-25 09:03:00 +0000

    Fix up DOM generation

--- react-poc/src/test.js
+++ react-poc/src/test.js
@@ -1,5 +1,13 @@
 // from dataToTree.js
-var input = {"ids":[1,2],"depth":1,"children":[{"ids":[3,5],"depth":2,"children":[{"ids":[6],"depth":3,"children":[]}]},{"ids":[4,7],"depth":2,"children":[]}]};
+var input = {
+    "ids": [1, 2],
+    "depth": 1,
+    "children": [{"ids": [3, 5], "depth": 2, "children": [{"ids": [6], "depth": 3, "children": []}]}, {
+        "ids": [4, 7],
+        "depth": 2,
+        "children": []
+    }]
+};
 
 //var input = {
 //    "ids": [1],
@@ -21,33 +29,39 @@ class App extends React.Component {
     }
 
     buildTree(node) {
-        var self = this;
+        function clicky(e, value) {
+            e.preventDefault();
+            window.alert(value);
+        }
 
-        // array of this node + siblings
+        // all the sibblings
         var nodes = node.ids.map((i) =>
-            <a href="#" key={i}>{i}</a>
+            <a href="#" key={i} onClick={(e) => clicky(e, i)}>{i}</a>
         );
 
-        var children = [];
+        // ... recurse the children
+        var self = this;
+        var childrenNodes = [];
         if (node.children.length > 0) {
             node.children.forEach(function (x) {
-                children.push(self.buildTree(x));
+                childrenNodes.push(self.buildTree(x));
             });
         }
 
-        console.log('nodes');
-        console.log(nodes);
-        console.log('children');
-        console.log(children);
+        // ... don't render child ul's if no data is present
+        var children = childrenNodes.length > 0 ? <ul>{ childrenNodes }</ul> : "";
 
+        // for react, need to give each nested element a unique 'key'
         return <li key={ this.idx++ }>
-                { nodes }
-                { children.length > 0 ? <ul>{ children }</ul> : "" }
-            </li>;
+            { nodes }
+            { children }
+        </li>;
     }
 
     render() {
-        return <div className="tree"><ul>{ this.state.tree }</ul></div>;
+        return <div className="tree">
+            <ul>{ this.state.tree }</ul>
+        </div>;
     }
 }
 
